d2d0181723350a9ded5f2f70bc32ec9839d8b0b9
Andre Lorbach <alorbach@adiscon.com>
2025-06-16 14:11:35 +0000

    Add PR file filter to skip builds for docs-only changes
    Introduced logic in RsyslogGitHubEventHandler to inspect changed files
    in GitHub pull requests and skip builds when only documentation files
    (e.g., *.md, *.txt, ChangeLog) are modified. A new method `_get_pr_files`
    uses the GitHub API to fetch changed files, while `_should_skip_files`
    matches them against ignore patterns.
    
    Build status is posted to GitHub via `_post_github_status`. The global
    GitHub token is now accessed via the initialized `g` variable from
    `init_globals`. Logging has been added for better traceability.
    
    This change helps reduce unnecessary CI builds and provides feedback
    directly on the PR.

--- master/master_includes.py
+++ master/master_includes.py
@@ -13,6 +13,14 @@ from buildbot.schedulers.forcesched import ForceScheduler
 from buildbot.changes import filter
 from buildbot.plugins import schedulers, util
 
+# Global reference to access secrets
+g = None
+
+# Function to initialize global variables
+def init_globals(globalvars_dict):
+	global g
+	g = globalvars_dict.get('g', {})
+
 # Local reference to c
 # lc = None
 
@@ -363,13 +371,27 @@ def appendBuilders(	lc, szRepoOwner, szRepoProject,
 ####### GITHUB HOOK
 import logging
 import re
+import fnmatch
 from twisted.internet import defer
 from dateutil.parser import parse as dateparse
 from buildbot.www.hooks.github import GitHubEventHandler
+from buildbot.util import httpclientservice
 from twisted.python import log
 
 # Custom GitHubEventHandler for RSyslog
 class RsyslogGitHubEventHandler(GitHubEventHandler):
+	# File patterns to ignore (similar to GitHub Actions paths-ignore)
+	IGNORE_PATTERNS = [
+		'ChangeLog',
+		'*.md',
+		'*.txt',
+		'*.rst',
+		'*.yaml',
+		'*.json',
+		'*.xml',
+		'*.html'
+	]
+
 	def _has_skip(self, msg):
 		# The message contains the skipping keyword no not.
 		# :return type: Bool
@@ -377,6 +399,94 @@ class RsyslogGitHubEventHandler(GitHubEventHandler):
 			if re.search(skip, msg):
 				return True
 		return False
+
+	@defer.inlineCallbacks
+	def _get_pr_files(self, repo, pr_number):
+		'''
+		Get list of files changed in a PR
+		:param repo: the repo full name, ``{owner}/{project}``
+		:param pr_number: PR number
+		:return: list of file paths
+		'''
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Getting PR files for repo: {}, PR: {}".format(repo, pr_number))
+		
+		headers = {
+			'User-Agent': 'Buildbot'
+		}
+		if g['secret_GITHUB_TOKEN']:
+			headers['Authorization'] = 'token ' + g['secret_GITHUB_TOKEN']
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Using GitHub token authentication")
+		else:
+			log.msg("RsyslogGitHubEventHandler: DEBUG - No GitHub token found")
+
+		url = '/repos/{}/pulls/{}/files'.format(repo, pr_number)
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Making API call to: {}".format(url))
+		
+		http = yield httpclientservice.HTTPClientService.getService(
+			self.master, self.github_api_endpoint, headers=headers,
+			debug=self.debug, verify=self.verify)
+		res = yield http.get(url)
+		log.msg("RsyslogGitHubEventHandler: DEBUG - API response status: {}".format(res.code))
+		
+		data = yield res.json()
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Raw API response data: {}".format(str(data)[:500] + "..." if len(str(data)) > 500 else str(data)))
+		
+		files = [f['filename'] for f in data]
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Extracted filenames: {}".format(files))
+		defer.returnValue(files)
+
+	def _should_skip_files(self, files):
+		'''
+		Check if PR should be skipped based on changed files
+		:param files: list of file paths
+		:return: True if all files match ignore patterns
+		'''
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Checking if files should be skipped")
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Files to check: {}".format(files))
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Ignore patterns: {}".format(self.IGNORE_PATTERNS))
+		
+		if not files:
+			log.msg("RsyslogGitHubEventHandler: DEBUG - No files found, not skipping")
+			return False
+			
+		non_ignored_files = []
+		ignored_files = []
+		
+		for filename in files:
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Checking file: {}".format(filename))
+			
+			# Check if file matches any ignore pattern
+			matches_ignore = False
+			for pattern in self.IGNORE_PATTERNS:
+				# Check both full path and basename
+				full_match = fnmatch.fnmatch(filename, pattern)
+				basename_match = fnmatch.fnmatch(filename.split('/')[-1], pattern)
+				
+				log.msg("RsyslogGitHubEventHandler: DEBUG - Pattern '{}' vs '{}': full_match={}, basename_match={}".format(
+					pattern, filename, full_match, basename_match))
+				
+				if full_match or basename_match:
+					matches_ignore = True
+					log.msg("RsyslogGitHubEventHandler: DEBUG - File '{}' matches ignore pattern '{}'".format(filename, pattern))
+					break
+			
+			# Categorize files
+			if matches_ignore:
+				ignored_files.append(filename)
+			else:
+				non_ignored_files.append(filename)
+				log.msg("RsyslogGitHubEventHandler: DEBUG - File '{}' does NOT match any ignore pattern".format(filename))
+		
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Summary: {} ignored files, {} non-ignored files".format(
+			len(ignored_files), len(non_ignored_files)))
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Ignored files: {}".format(ignored_files))
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Non-ignored files: {}".format(non_ignored_files))
+		
+		# If any file doesn't match ignore patterns, don't skip
+		should_skip = len(non_ignored_files) == 0
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Should skip build: {}".format(should_skip))
+		
+		return should_skip
 
 	def _get_commit_msg(self, repo, sha):
 		'''
@@ -386,8 +496,8 @@ class RsyslogGitHubEventHandler(GitHubEventHandler):
 		headers = {
 			'User-Agent': 'Buildbot'
 		}
-		if self._token:
-			headers['Authorization'] = 'token ' + self._token
+		if g['secret_GITHUB_TOKEN']:
+			headers['Authorization'] = 'token ' + g['secret_GITHUB_TOKEN']
 
 		url = '/repos/{}/commits/{}'.format(repo, sha)
 		http = yield httpclientservice.HTTPClientService.getService(
@@ -398,6 +508,7 @@ class RsyslogGitHubEventHandler(GitHubEventHandler):
 		msg = data['commit']['message']
 		defer.returnValue(msg)
 
+	@defer.inlineCallbacks
 	def handle_pull_request(self, payload, event):
 		changes = []
 		
@@ -416,6 +527,51 @@ class RsyslogGitHubEventHandler(GitHubEventHandler):
 		head_sha = payload['pull_request']['head']['sha']
 		
 		log.msg('RsyslogGitHubEventHandler: Processing GitHub PR #{}'.format(number), logLevel=logging.DEBUG)
+
+		# Check if only documentation files are changed
+		log.msg("RsyslogGitHubEventHandler: DEBUG - Starting file filtering check for PR #{}".format(number))
+		try:
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Calling _get_pr_files for repo: {}, PR: {}".format(repo_full_name, number))
+			pr_files = yield self._get_pr_files(repo_full_name, number)
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Successfully retrieved {} files for PR #{}".format(len(pr_files), number))
+			log.msg("RsyslogGitHubEventHandler: ALL FILES CHANGED IN PR #{}: {}".format(number, pr_files))
+			
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Calling _should_skip_files with {} files".format(len(pr_files)))
+			should_skip = self._should_skip_files(pr_files)
+			log.msg("RsyslogGitHubEventHandler: DEBUG - _should_skip_files returned: {}".format(should_skip))
+			
+			if should_skip:
+				log.msg("RsyslogGitHubEventHandler: GitHub PR #{}, SKIPPING BUILD: only documentation files changed".format(number))
+				
+				# Post status to GitHub indicating build was skipped
+				yield self._post_github_status(
+					repo_full_name, 
+					head_sha, 
+					'success', 
+					'Build skipped - only documentation files changed',
+					'buildbot/file-filter'
+				)
+				
+				log.msg("RsyslogGitHubEventHandler: DEBUG - Returning empty changes list to skip build")
+				defer.returnValue(([], 'git'))
+			else:
+				log.msg("RsyslogGitHubEventHandler: GitHub PR #{}, PROCEEDING WITH BUILD: non-documentation files changed".format(number))
+				
+				# Post status to GitHub indicating build will proceed
+				yield self._post_github_status(
+					repo_full_name, 
+					head_sha, 
+					'success', 
+					'Build triggered - code files changed',
+					'buildbot/file-filter'
+				)
+				
+		except Exception as e:
+			log.msg("RsyslogGitHubEventHandler: ERROR - Could not check PR files for #{}: {}".format(number, str(e)))
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Exception details: {}".format(repr(e)))
+			import traceback
+			log.msg("RsyslogGitHubEventHandler: DEBUG - Traceback: {}".format(traceback.format_exc()))
+			# Continue with build if file check fails
 
 		head_msg = yield self._get_commit_msg(repo_full_name, head_sha)
 		'''
@@ -461,3 +617,53 @@ class RsyslogGitHubEventHandler(GitHubEventHandler):
 		# Do some magic here
 		defer.returnValue((changes, 'git'))
 		#return [changes], 'git'
+
+	@defer.inlineCallbacks
+	def _post_github_status(self, repo, sha, state, description, context="buildbot/file-filter"):
+		"""
+		Post a status update to GitHub
+		:param repo: the repo full name, ``{owner}/{project}``
+		:param sha: commit SHA
+		:param state: one of 'error', 'failure', 'pending', 'success'
+		:param description: status description
+		:param context: status context (appears in PR checks)
+		"""
+		log.msg("RsyslogGitHubEventHandler: Posting GitHub status - repo: {}, sha: {}, state: {}, context: {}".format(
+			repo, sha, state, context))
+		
+		headers = {
+			'User-Agent': 'Buildbot',
+			'Accept': 'application/vnd.github.v3+json'
+		}
+		if g['secret_GITHUB_TOKEN']:
+			headers['Authorization'] = 'token ' + g['secret_GITHUB_TOKEN']
+			log.msg("RsyslogGitHubEventHandler: Using GitHub token for status update")
+		else:
+			log.msg("RsyslogGitHubEventHandler: WARNING - No GitHub token found for status update")
+			defer.returnValue(None)
+
+		url = '/repos/{}/statuses/{}'.format(repo, sha)
+		payload = {
+			'state': state,
+			'description': description,
+			'context': context
+		}
+		
+		log.msg("RsyslogGitHubEventHandler: Making status API call to: {} with payload: {}".format(url, payload))
+		
+		try:
+			http = yield httpclientservice.HTTPClientService.getService(
+				self.master, self.github_api_endpoint, headers=headers,
+				debug=self.debug, verify=self.verify)
+			res = yield http.post(url, json=payload)
+			log.msg("RsyslogGitHubEventHandler: Status API response: {}".format(res.code))
+			
+			if res.code in (200, 201):
+				log.msg("RsyslogGitHubEventHandler: Successfully posted GitHub status")
+			else:
+				log.msg("RsyslogGitHubEventHandler: GitHub status API returned code: {}".format(res.code))
+				
+		except Exception as e:
+			log.msg("RsyslogGitHubEventHandler: ERROR posting GitHub status: {}".format(str(e)))
+			import traceback
+			log.msg("RsyslogGitHubEventHandler: Status error traceback: {}".format(traceback.format_exc()))
